90a17e21413821f804119b61e7464a1323d7aecb
Seervisual <seervisual@gmail.com>
2025-03-28 14:54:52 +1100

    stable_V03_redirect added

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Use 'standalone' for server-side rendering
-  distDir: '.next', // Matches the SSR output directory
+  output: 'export', // Use 'export' for static site generation
+  distDir: 'out', // Change the output directory to 'out'
   images: {
     unoptimized: true,
   },
   transpilePackages: ["lucide-react"],
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
+  assetPrefix: './', // Use './' for relative paths in static export
 };
 
 export default nextConfig;
